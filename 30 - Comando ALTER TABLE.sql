ba5a484e3e366e1cc5c5dd73464050ae1275a114
MAXWEL SILVA DOS SANTOS <63561558+maxwel-silva@users.noreply.github.com>
2022-10-02 22:07:39 -0300

    Update 30 - Comando ALTER TABLE.sql

--- 30 - Comando ALTER TABLE.sql
+++ 30 - Comando ALTER TABLE.sql
@@ -7,6 +7,5 @@ SELECT * FROM AdventureWorks.HumanResources.JobCandidate
 ALTER TABLE AdventureWorks.HumanResources.JobCandidate
 ADD Ativo BIT
 
-
 ALTER TABLE AdventureWorks.HumanResources.JobCandidate
 ALTER COLUMN Ativo VARCHAR (100) NOT NULL

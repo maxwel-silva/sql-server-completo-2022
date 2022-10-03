5e66eb0e636dc88da656beb52882d40ef9617406
MAXWEL SILVA DOS SANTOS <63561558+maxwel-silva@users.noreply.github.com>
2022-10-02 22:14:05 -0300

    Update 09 - Comando IN.sql

--- 09 - Comando IN.sql
+++ 09 - Comando IN.sql
@@ -8,4 +8,3 @@ WHERE BusinessEntityID IN (2, 7, 12)
 SELECT * FROM Person.Person
 WHERE PersonType NOT IN ('EM', 'SP', 'SC', 'VC') 
 
---------------------------------------------------------------------------------------------
